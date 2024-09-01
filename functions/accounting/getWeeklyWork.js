695052907e1b3a75dfa8951ba02fd06a1decc052
GeorgeEu <g.zanevski@gmail.com>
2024-09-02 01:33:18 +0400

    test time trigerred fun

---
+++ functions/accounting/getWeeklyWork.js
@@ -0,0 +1,7 @@
+// accounting/getWeeklyWork.js
+
+module.exports = async (context) => {
+  console.log("Weekly work function executed!");
+  // Add your logic here
+  return null; // Return a promise or value as needed
+};
--- functions/index.js
+++ functions/index.js
@@ -6,6 +6,12 @@ admin.initializeApp();
 
 // Import functions from the accounting directory
 const getLogs = require("./accounting/getLogs");
+const getWeeklyWork = require("./accounting/getWeeklyWork");
 
 // Export the functions
 exports.getLogs = functions.https.onRequest(getLogs);
+
+// Schedule the getWeeklyWork function to run every week
+exports.getWeeklyWork = functions.pubsub
+    .schedule("every 24 hours")
+    .onRun(getWeeklyWork);
